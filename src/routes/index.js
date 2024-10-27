17a3d257d65b413973f2c13517c52ba78332da0f
thallyta-castro-cv <thallyta180136319@gmail.com>
2024-10-27 19:13:48 -0300

    feat: incluido rota de autenticação no index

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,11 @@ import express from "express";
 import books from "./booksRoutes.js";
 import authors from "./authorRoutes.js";
 import users from "./userRoutes.js"
+import auth from "./authRoutes.js"
 
 const routes = (app) => {
   app.route("/").get((req, res) => res.status(200).send("Curso de Node.js"));
-  app.use(express.json(), books, authors, users);
+  app.use(express.json(), books, authors, users, auth);
 };
 
 export default routes;
